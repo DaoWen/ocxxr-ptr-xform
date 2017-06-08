dafdf6514adb99a157dfa3ec6c8667b967b60cc6
Nick Vrvilo <Nick.Vrvilo@rice.edu>
2017-06-07 22:17:39 -0500

    Find all TaskBuilder Datablock payload types

--- src_clang/ocxxr/taskbuilder_matcher.cpp
+++ src_clang/ocxxr/taskbuilder_matcher.cpp
@@ -1,6 +1,9 @@
 //------------------------------------------------------------------------------
+#include <typeinfo>
+
 #include <set>
 #include <string>
+#include <cstring>
 
 #include "clang/AST/AST.h"
 #include "clang/AST/ASTConsumer.h"
@@ -13,6 +16,18 @@
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Tooling.h"
 #include "llvm/Support/raw_ostream.h"
+
+#define ENABLE_DEBUG 1
+
+#define ASSERT(cond) do { \
+  if (ENABLE_DEBUG) { \
+    if (!(cond)) { \
+      llvm::errs() << "ASSERT FAILED: " << (#cond) << "\n" \
+      << "\tat " << __FILE__ << ":" << __LINE__ << "\n"; \
+      std::abort(); \
+    } \
+  } \
+} while (0)
 
 namespace clang {
 namespace ast_matchers {
@@ -36,14 +51,62 @@ using namespace clang::tooling;
 
 static llvm::cl::OptionCategory ToolingSampleCategory("Matcher Sample");
 
+static bool StringStartsWith(const std::string &str, const std::string &prefix) {
+  return str.size() >= prefix.size() && str.compare(0, prefix.size(), prefix) == 0;
+}
+
+static const std::string ocxxr_db_prefix = "class ocxxr::Datablock<";
+
+static bool IsDatablockType(const QualType &type) {
+    if (!type->isClassType()) return false;
+    const QualType &raw_type = type->getCanonicalTypeUnqualified();
+    return StringStartsWith(raw_type.getAsString(), ocxxr_db_prefix);
+}
+
+static QualType UnpackDbType(const QualType &db_type) {
+  auto *record = db_type->getAsCXXRecordDecl();
+  auto *specialization = llvm::dyn_cast<ClassTemplateSpecializationDecl>(record);
+  ASSERT(specialization);
+  auto &template_args = specialization->getTemplateArgs();
+  ASSERT(template_args.size() == 1);
+  return template_args[0].getAsType();
+}
+
+static void FindPersistedClassTypes(llvm::raw_ostream &out, const ClassTemplateSpecializationDecl &d) {
+  auto &template_args = d.getTemplateArgs();
+  ASSERT(template_args.size() > 0);
+  auto const &arg_f = template_args[0].getAsType();
+  ASSERT(arg_f->isFunctionProtoType());
+  auto const &fn_type = *arg_f->getAs<FunctionProtoType>();
+  const size_t args_count = fn_type.getNumParams();
+  for (size_t i=0; i<args_count; i++) {
+    const QualType &type = fn_type.getParamType(i);
+    const bool is_db = IsDatablockType(type);
+    if (is_db) {
+      //QualType.getBaseTypeIdentifier
+      //Type.canDecayToPointerType
+      const QualType inner_type = UnpackDbType(type);
+      out << "    DB: " << inner_type.getAsString() << "\n";
+    }
+    else {
+      out << "    " << type.getAsString() << "\n";
+    }
+  }
+}
+
 struct StuffDumper : public MatchFinder::MatchCallback {
   virtual void run(const MatchFinder::MatchResult &Result) {
     auto *d = Result.Nodes.getNodeAs<ClassTemplateSpecializationDecl>("stuff");
-    d->dump();
+    //d->dump();
     const SourceManager &SM = Result.Context->getSourceManager();
     auto loc = d->getPointOfInstantiation();
     if (loc.isValid()) {
-      loc.print(llvm::outs(), SM);
+      auto &out = llvm::outs();
+      out << "Found task template!\n  Location:\n    ";
+      loc.print(out, SM);
+      out << "\n  Types:\n";
+      FindPersistedClassTypes(out, *d);
+      out << "\n";
     }
   }
 };
@@ -59,7 +122,7 @@ int main(int argc, const char **argv) {
   const TypeMatcher AnyType = anything();
 
   Finder.addMatcher(
-      classTemplateSpecializationDecl(hasName("::ocxxr::TaskTemplate"))
+      classTemplateSpecializationDecl(hasName("::ocxxr::TaskBuilder"))
       .bind("stuff"),
       &HandlerForStuff);
 
